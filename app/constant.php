dfaccd2dea6f332a389aab0bf56624fe466780c2
Rashmi Dholakiya <rashmi.dholakiya@arsenaltech.com>
2016-03-28 17:59:58 +0530

    constant

--- app/constant.php
+++ app/constant.php
@@ -2,9 +2,11 @@
 define('STATUS_OK', 200);
 define('EMS_API_PATH', 'ems/ems_api/');
 
+define('HOST','http://localhost');
+
 define('GOOGLE_ID','653019171081-i4gs357133d83c2uf55d4nr486n3s8hn.apps.googleusercontent.com');
 define('GOOGLE_SECRET','hduxPX8wfkfFL3gMMxfrTofm');
-define('GOOGLE_REDIRECT','/auth/google/callback');
+define('GOOGLE_REDIRECT', HOST.'folio/public/auth/google/callback');
 
 define('PAGINATE_LIMIT', 15);
 
