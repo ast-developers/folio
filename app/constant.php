a2c705a6aa7af8743da407611602d5fea74b72a5
Rashmi Dholakiya <rashmi.dholakiya@arsenaltech.com>
2016-03-28 18:17:48 +0530

    date-range

--- app/constant.php
+++ app/constant.php
@@ -6,7 +6,7 @@ define('HOST', 'http://projects.arsenaltech.com');
 
 define('GOOGLE_ID','653019171081-i4gs357133d83c2uf55d4nr486n3s8hn.apps.googleusercontent.com');
 define('GOOGLE_SECRET','hduxPX8wfkfFL3gMMxfrTofm');
-define('GOOGLE_REDIRECT', HOST.'folio/public/auth/google/callback');
+define('GOOGLE_REDIRECT', HOST.'/folio/public/auth/google/callback');
 
 define('PAGINATE_LIMIT', 15);
 
