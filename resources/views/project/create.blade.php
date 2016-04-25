c8ad43769010d8e38463ec1ce613fb2b63d77f54
Rashmi Dholakiya <rashmi.dholakiya@arsenaltech.com>
2016-04-25 14:51:56 +0530

    Remove Individual messages from add new project form

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -11,28 +11,24 @@
                 {!! Form::label('name', 'Name: ', ['class' => 'col-sm-3 control-label']) !!}
                 <div class="col-sm-6">
                     {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                    {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
             <div class="form-group {{ $errors->has('jira_key') ? 'has-error' : ''}}">
                 {!! Form::label('jira_key', 'Jira Key: ', ['class' => 'col-sm-3 control-label']) !!}
                 <div class="col-sm-6">
                     {!! Form::text('jira_key', null, ['class' => 'form-control']) !!}
-                    {!! $errors->first('jira_key', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
             <div class="form-group {{ $errors->has('start_date') ? 'has-error' : ''}}">
                 {!! Form::label('start_date', 'Start Date: ', ['class' => 'col-sm-3 control-label' ]) !!}
                 <div class="col-sm-6">
                     {!! Form::date('start_date', null, ['class' => 'form-control datepicker', 'placeholder'=>'YYYY-MM-DD']) !!}
-                    {!! $errors->first('start_date', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
             <div class="form-group {{ $errors->has('end_date') ? 'has-error' : ''}}">
                 {!! Form::label('end_date', 'End Date: ', ['class' => 'col-sm-3 control-label']) !!}
                 <div class="col-sm-6">
                     {!! Form::date('end_date', null, ['class' => 'form-control datepicker', 'placeholder'=>'YYYY-MM-DD']) !!}
-                    {!! $errors->first('end_date', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
             <div class="form-group {{ $errors->has('is_overhead') ? 'has-error' : ''}}">
@@ -44,7 +40,6 @@
             <div class="checkbox">
                 <label>{!! Form::radio('is_overhead', '0', true) !!} No</label>
             </div>
-                    {!! $errors->first('is_overhead', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
 
