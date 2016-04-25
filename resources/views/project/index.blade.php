739f2bf4bde6e97b2d0756e67ca39d24a316f1ea
Rashmi Dholakiya <rashmi.dholakiya@arsenaltech.com>
2016-04-25 17:17:57 +0530

    Use of constant

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <h1>Projects
-        @if( Auth::user()->role_id != 3)
+        @if( Auth::user()->role_id != THREE)
         <a href="{{ route('project.create') }}" class="btn btn-primary pull-right btn-sm">Add New Project</a>
         @endif
     </h1>
@@ -11,7 +11,7 @@
         <table class="table table-bordered table-striped table-hover">
             <thead>
                 <tr>
-                    <th>S.No</th><th>Name</th><th>Jira Key</th><th>Start Date</th> @if( Auth::user()->role_id != 3)<th>Actions</th>@endif
+                    <th>S.No</th><th>Name</th><th>Jira Key</th><th>Start Date</th> @if( Auth::user()->role_id != THREE)<th>Actions</th>@endif
                 </tr>
             </thead>
             <tbody>
@@ -25,7 +25,7 @@
                         <td><a href="{{ url('/project', $item->id) }}">{{ $item->name }}</a></td>
                         <td>{{ $item->jira_key}}</td>
                         <td>{{ date_formation($item->start_date) }}</td>
-                        @if( Auth::user()->role_id != 3)
+                        @if( Auth::user()->role_id != THREE)
                         <td>
                             <a href="{{ route('project.edit', $item->id) }}">
                                 <button type="submit" class="btn btn-primary btn-xs">Update</button>
@@ -35,7 +35,7 @@
                                 'route' => ['project.destroy', $item->id],
                                 'style' => 'display:inline'
                             ]) !!}
-                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs']) !!}
+                            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-xs delete']) !!}
                             {!! Form::close() !!}
                         </td>
                          @endif
