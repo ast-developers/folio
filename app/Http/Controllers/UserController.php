9bf9807723633ccd7a1dfa7feca5bbdc027e5821
malaymehta <malay.mehta@arsenaltech.com>
2016-08-22 14:20:00 +0530

    Changes: - Resolved the issue for saving the user.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Events\SendMail;
@@ -16,35 +15,32 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
-
-
 use Socialite;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
-
 class UserController extends Controller
 {
-	protected $userRepository;
-	protected $projectRepository;
+    protected $userRepository;
+    protected $projectRepository;
 
-	public function __construct(UserRepositoryInterface $usersRepository, ProjectRepositoryInterface $projectRepository)
-	{
-		$this->userRepository = $usersRepository;
-		$this->projectRepository = $projectRepository;
-	}
+    public function __construct(UserRepositoryInterface $usersRepository, ProjectRepositoryInterface $projectRepository)
+    {
+        $this->userRepository = $usersRepository;
+        $this->projectRepository = $projectRepository;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-
     public function index()
     {
-		$users = $this->userRepository->getUsers();
-		return view('user.index', compact('users'));
+        $users = $this->userRepository->getUsers();
+        return view('user.index', compact('users'));
     }
 
     /**
@@ -54,92 +50,91 @@ class UserController extends Controller
      */
     public function create()
     {
-		$user_roles = UserRoles::lists('user_role_name', 'id')->toArray();
-		$projects = $this->projectRepository->getAllProjects()->lists('name', 'id');
-		return view('user.create', compact('user_roles', 'projects'));
+        $user_roles = UserRoles::lists('user_role_name', 'id')->toArray();
+        $projects = $this->projectRepository->getAllProjects()->lists('name', 'id');
+        return view('user.create', compact('user_roles', 'projects'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  UserRequest  $request
+     * @param  UserRequest $request
      * @return \Illuminate\Http\Response
      */
     public function store(UserRequest $request)
     {
-		$user = $this->userRepository->save($request->all());
-		Event::fire(new SendMail($user));
-		Session::flash('message', 'User successfully added!');
-		return redirect('user');
+        $user = $this->userRepository->save($request->all());
+        Event::fire(new SendMail($user));
+        Session::flash('message', 'User successfully added!');
+        return redirect('user');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-		$user = User::find($id);
-		return view('user.show', compact('user'));
+        $user = User::find($id);
+        return view('user.show', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
     {
-		$selected_project_list = null;
-		$user     = $this->userRepository->getUserByIdWithRole($id);
-		$projects = $this->projectRepository->getAllProjects()->lists('name', 'id');
-		$selected_project_list = $this->projectRepository->getSelectedProjectList($user);
-
-		return view('user.edit', compact('user', 'projects', 'selected_project_list'));
+        $selected_project_list = null;
+        $user = $this->userRepository->getUserByIdWithRole($id);
+        $projects = $this->projectRepository->getAllProjects()->lists('name', 'id');
+        $selected_project_list = $this->projectRepository->getSelectedProjectList($user);
+        return view('user.edit', compact('user', 'projects', 'selected_project_list'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int                      $id
      * @return \Illuminate\Http\Response
      */
     public function update(UserRequest $request, $id)
     {
-		$this->userRepository->save($request->all(), $id);
-		Session::flash('message', 'User successfully updated!');
-		return redirect('user');
+        $this->userRepository->save($request->all(), $id);
+        Session::flash('message', 'User successfully updated!');
+        return redirect('user');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-		User::find($id)->projects()->detach($id);
-		if (User::destroy($id)) {
-			Session::flash('message', 'User successfully updated!');
-			return Redirect::back();
-		} else {
-			return redirect('user');
-		}
-
+        User::find($id)->projects()->detach($id);
+        if (User::destroy($id)) {
+            Session::flash('message', 'User successfully updated!');
+            return Redirect::back();
+        } else {
+            return redirect('user');
+        }
     }
-	public function setPassword($email, $token)
-	{
-		$user = User::where('remember_token', $token)->where('email', $email)->first();
-		if ($user) {
-			return view('errors.unauthorized');
-		}
-		return view('password.reset', compact('email', 'token'));
-	}
+
+    public function setPassword($email, $token)
+    {
+        $user = User::where('remember_token', $token)->where('email', $email)->first();
+        if ($user) {
+            return view('errors.unauthorized');
+        }
+        return view('password.reset', compact('email', 'token'));
+    }
 
     public function googlehandle()
     {
@@ -148,10 +143,8 @@ class UserController extends Controller
         } catch (Exception $e) {
             return redirect('auth/google');
         }
-
         $authUser = $this->findOrCreateUser($user);
-        if(!$authUser)
-        {
+        if (!$authUser) {
             Session::flash('message', 'You are not a member of Arsenal Team. or You are not authorized uder. So you can not login');
             return Redirect::to('/auth/login');
         }
@@ -161,21 +154,18 @@ class UserController extends Controller
 
     private function findOrCreateUser($google_user)
     {
-        $user =new User();
-        //$user = User::where('email', $google_user->email)->first();
-
-        //if ($user) {
-            $user->name         = $google_user->name;
-            $user->email         = $google_user->email;
-            $user->google_id    = $google_user->id;
-            $user->avatar       = $google_user->avatar;
+        $user = User::where('email', $google_user->email)->firstOrFail();
+        if (!$user) {
+            $user->name = $google_user->name;
+            $user->email = $google_user->email;
+            $user->google_id = $google_user->id;
+            $user->avatar = $google_user->avatar;
             $user->access_token = $google_user->token;
-            $user->saveOrFail();
-            return $user;
-        /*}
-        else{
+            $user->save();
+        }
+        return $user;
+        /* else{
             return false;
         }*/
     }
-
 }
